0e8d831a7d5829da7a463800306013abc7cd52b2
Rey-Of-Sunshine <msd.21@uni-dubna.ru>
2022-11-10 00:55:07 +0300

    fgh

--- app.js
+++ app.js
@@ -12,4 +12,15 @@ document.body.onload = () => {
             document.body.innerHTML = request.responseText + document.body.innerHTML
         }
     }
+    let requestfoot = new XMLHttpRequest;
+    requestfoot.open('GET', './footer.html', true)
+    requestfoot.send()
+    requestfoot.onreadystatechange = function () {
+        if (requestfoot.readyState === 4)
+        if (requestfoot.status === 200) {
+            console.log(request.responseText)
+            document.body.innerHTML = document.body.innerHTML + requestfoot.responseText
+        }
+    }
 }
+
